8e992e8b04c510a20126058fe661f9644ff28200
Abdullah Zezo <abdullahzezo@stud.fci-cu.edu.eg>
2020-10-10 01:43:33 +0200

    fsfd

---
+++ WebApplication1/WebApplication1/Migrations/202010080152109_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        address = c.String(),
+                        age = c.Int(nullable: false),
+                        gender = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        u_username = c.String(),
+                        u_password = c.String(),
+                        u_name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.users");
+            DropTable("dbo.customers");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Migrations/202010092332508_AddedNew.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedNew : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.customers", "Done", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.customers", "Done");
+        }
+    }
+}
